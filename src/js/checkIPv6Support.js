c6c7c1375fc41f742b1ec79f5eba4ee9fa1bc746
HeroLeam <thiagoleam@gmail.com>
2024-08-15 20:59:53 -0300

    update

--- src/js/checkIPv6Support.js
+++ src/js/checkIPv6Support.js
@@ -3,19 +3,28 @@ function checkIPv6Support() {
 
     fetch(ipv6TestUrl, { mode: 'no-cors' })
         .then(function (response) {
-            // showMessage("Você tem suporte a IPv6!");
+            showMessageTrue("Você tem suporte IPv6!");
         })
         .catch(function (error) {
-            showMessage("Você não tem IPv6 ativo.\n" + "Entre em contato com seu provedor para estar fazendo a ativação do protocolo IPv6.");
+            showMessageFalse("Você não tem IPv6 ativo.\n" + "Entre em contato com seu provedor para estar fazendo a ativação do protocolo IPv6.");
         });
 }
 
-function showMessage(message) {
+function showMessageTrue(message) {
     var messageElement = document.createElement("p");
     messageElement.textContent = message;
-    messageElement.classList.add('ipv6');
+    messageElement.classList.add('ipv6True');
     
-    var headerElement = document.querySelector('header');
+    var headerElement = document.querySelector('.paragrafo');
+    headerElement.parentNode.insertBefore(messageElement, headerElement.nextSibling);
+}
+
+function showMessageFalse(message) {
+    var messageElement = document.createElement("p");
+    messageElement.textContent = message;
+    messageElement.classList.add('ipv6False');
+    
+    var headerElement = document.querySelector('.paragrafo');
     headerElement.parentNode.insertBefore(messageElement, headerElement.nextSibling);
 }
 
