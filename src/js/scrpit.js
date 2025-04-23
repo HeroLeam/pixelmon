02f9b9fac2bb0c7cddcff856e5ed6da53a763ad0
Thiago Leoni Amaral <thiagoleam@gmail.com>
2025-04-23 15:14:28 -0300

    Update

--- src/js/scrpit.js
+++ src/js/scrpit.js
@@ -1,4 +1,27 @@
 function toggleMenu() {
-    const menu = document.getElementById("menu");
-    menu.classList.toggle("show");
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("show");
+}
+
+async function checkServer() {
+  const ip = "herofield.com.br:25567";
+
+  try {
+    const response = await fetch(`https://api.mcstatus.io/v2/status/java/${ip}`);
+    const data = await response.json();
+
+    const statusDiv = document.getElementById("mcStatus");
+    if (data.online) {
+      statusDiv.innerText = `🟢 Online - ${data.players.online}/${data.players.max} jogadores!`;
+      statusDiv.className = "online";
+    } else {
+      statusDiv.innerText = "🔴 Servidor Offline";
+      statusDiv.className = "offline";
+    }
+  } catch (error) {
+    document.getElementById("mcStatus").innerText = "Erro ao verificar o status";
   }
+}
+
+checkServer();
+setInterval(checkServer, 30000);
